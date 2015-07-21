46973b0683d11dd61bdb3b24c018a32c63ac1788
Lethallic <lethallic@gmail.com>
2015-07-21 15:46:58 +0200

    ...

--- src/config.global.js
+++ src/config.global.js
@@ -2,18 +2,22 @@ function applyMiner(rc) {
 	if (Game.time % 5 !== 0) return;
 
 	var transporters = rc.getCreeps("transporter");
+	if (transporters.length === 0) return;
+
 	var miners = rc.getCreeps("miner");
 	var perMiner = transporters.length / miners.length;
 
 	for (var t in transporters) {
 		var creep = transporters[t];
-    
+
 		for (var m in miners) {
 			var miner = miners[m];
 
-			var tm = _.filter(transporters, function (t) {
-    		return (t.memory.miner == miner.id);
-    	});
+			var tm = _.filter(transporters, { 'memory' : { 'miner' : miner.id } });
+
+			// var tm = _.filter(transporters, function (t) {
+			// 	return (t.memory.miner == miner.id);
+			// });
 
 			if (tm.length < perMiner) {
 				creep.memory.miner = miner.id;
@@ -27,7 +31,7 @@ function applyMiner(rc) {
 module.exports = {
 	rooms: {
 		init: function (rc) {
-      applyMiner(rc);
-    }
+			applyMiner(rc);
+		}
 	}
 };
