54117165779d8238ec3599affc77f038110b087a
utiuti <marcel.uthoff@gmail.com>
2015-08-13 19:44:07 +0200

    ToDo angepasst

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
-// To - DO
-// - During game start. Constructors try to repair protection wall with 1/1 hit. Why?
+// To - DO:
+// X FIXED - During game start. Constructors try to repair protection wall with 1/1 hit. Why?
 // - Builder Creeps just harvest 1 source. 
 // - Builders can block themselves if trying to upgrade controller
 // - Buildscreeps can fail if there is not much energy and 3 constructors are building. Takes a long time until 1 builder gets the job done. (No building if miner/transporter is 0)
