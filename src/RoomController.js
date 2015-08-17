5345a97cf2cd35245c6d88053a18e24b0a3020d1
utiuti <marcel.uthoff@gmail.com>
2015-08-17 19:27:16 +0200

    bin zu blöd für debug

--- src/RoomController.js
+++ src/RoomController.js
@@ -174,7 +174,7 @@ RoomController.prototype.getLevel = function () {
 RoomController.prototype.getIdleSpawn = function () {
 	for (var i in this._spawns) {
 		var sc = this._spawns[i];
-		console.log(sc);
+		console.log(i);
 		if (sc.idle()) {
 			console.log("Idle: " + sc);
 			return sc;
