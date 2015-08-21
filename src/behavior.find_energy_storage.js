db6a094833010902a31ee89e35e4999ce59fbd82
utiuti <marcel.uthoff@gmail.com>
2015-08-21 21:02:39 +0200

    Storage Test

---
+++ src/behavior.find_energy_storage.js
@@ -0,0 +1,32 @@
+var Behavior = require("_behavior");
+
+var b = new Behavior("find_energy_storage");
+
+b.when = function(creep, rc) {
+  return (creep.energy === 0 && rc.room.storage);
+};
+
+b.completed = function(creep, rc) {
+  var target = Game.getObjectById(creep.target);
+  return (target === null || creep.energy === creep.energyCapacity);
+};
+
+b.work = function(creep, rc) {
+  var target = creep.getTarget();
+
+
+  if ( target === null ) {
+     creep.target = rc.room.storage.id;
+  }
+
+  if ( target !== null ) {
+    if ( !creep.pos.isNearTo(target) ) {
+      creep.moveToEx(target);
+    } else {
+      target.transferEnergy(creep);
+      creep.target = null;
+    }
+  }
+};
+
+module.exports = b;
