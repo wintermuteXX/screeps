6cf0c890c1eaad3b33942f2b2b89d0a3eea8c17a
Zimt <utiuti@users.noreply.github.com>
2015-08-27 08:13:03 +0200

    Update behavior.get_energy_link.js

--- src/behavior.get_energy_link.js
+++ src/behavior.get_energy_link.js
@@ -17,7 +17,7 @@ b.when = function(creep, rc) {
 
 b.completed = function(creep, rc) {
   var target = Game.getObjectById(creep.target);
-  return (target === null || creep.energy === creep.energyCapacity);
+  return (target === null || creep.energy > 0);
 };
 
 b.work = function(creep, rc) {
