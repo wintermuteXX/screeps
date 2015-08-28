953acd8205c687432870a893eb0af3c646f87d1d
utiuti <marcel.uthoff@gmail.com>
2015-08-28 20:24:24 +0200

    tst

--- src/behavior.find_near_energy.js
+++ src/behavior.find_near_energy.js
@@ -18,7 +18,7 @@ function findStorage(obj, rc) {
   var store = rc.room.storage;
   if ( !!store ) {
     var thestorage =  obj.pos.findInRange([store],3);
-    console.log("thestorage: " + thestorage + "store.energy " + thestorage.energy);
+    console.log("TheStorage: " + thestorage + " | TheStorage.energy " + thestorage.energy);
     if (thestorage.energy > 0) return thestorage;
   }
   return null;
