ce386404407c3c4c441f6971f68a6c3cb87e1c46
utiuti <marcel.uthoff@gmail.com>
2015-09-02 17:58:58 +0200

    anpassung -> pos

--- src/behavior.goto_red_flag.js
+++ src/behavior.goto_red_flag.js
@@ -3,25 +3,25 @@ var Behavior = require("_behavior");
 var b = new Behavior("goto_red_flag");
 
 function findFlag(rc) {
-    return _.find(Game.flags, {
-      'color': COLOR_RED
-    });
+  return _.find(Game.flags, {
+    'color': COLOR_RED
+  });
 }
 
-b.when = function(creep, rc) {
+b.when = function (creep, rc) {
   var flag = findFlag(rc);
   return !!flag && flag.room !== creep.room;
 };
 
-b.completed = function(creep, rc) {
+b.completed = function (creep, rc) {
   var flag = findFlag(rc);
-  return !flag || flag.room === creep.room;
+  return !flag || flag.pos === creep.pos;
 };
 
-b.work = function(creep, rc) {
+b.work = function (creep, rc) {
   var flag = findFlag(rc);
   if (flag) {
-    creep.moveToEx(flag);
+    creep.moveToEx(flag, { ignoreDestructibleStructures: true });
   }
 };
 
