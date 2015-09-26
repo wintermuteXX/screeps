a4a1218068073672009cd40fb20bc9decaec3f1d
utiuti <marcel.uthoff@gmail.com>
2015-09-26 16:44:45 +0200

    test

--- src/behavior.get_energy_storage.js
+++ src/behavior.get_energy_storage.js
@@ -3,8 +3,8 @@ var Behavior = require("_behavior");
 var b = new Behavior("get_energy_storage");
 
 b.when = function(creep, rc) {
-  console.log(rc.room.storage.store);
-  return (creep.energy === 0 && rc.room.storage.store > 0);
+  console.log(rc.room.storage.store.energy);
+  return (creep.energy === 0 && rc.room.storage.store.energy > 0);
 };
 
 b.completed = function(creep, rc) {
