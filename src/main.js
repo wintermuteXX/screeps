9ee322ccd4f0fe22008de309e2ed642acc7147ed
utiuti <marcel.uthoff@gmail.com>
2016-01-28 20:21:14 +0100

    test

--- src/main.js
+++ src/main.js
@@ -5,7 +5,6 @@
 // X Buildscreeps can fail if there is not much energy and 3 constructors are building. Takes a long time until 1 builder gets the job done. (No building if miner/transporter is 0)
 // X Miner should automatically fill Link)
 // - find dropped energy choose target with random, even fail should be possible when there are links. And a minimun of dropped energy (>50
-// - Builders can block themselves if trying to upgrade controller
 // - Links automatisch bauen
 // - Storage automatisch bauen
 // - Extensions automatisch bauen
@@ -14,10 +13,7 @@
 require("_init");
 var profiler = require('screeps-profiler');
 
-
-
 profiler.enable();
-
   module.exports.loop = function() {
     profiler.wrap(function() {
     // Main.js logic should go here.
