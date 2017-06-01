369e6ad50ac0a1f1d09aa1d9999e85fd8975a86f
Zimt <marcel.uthoff@gmail.com>
2017-06-01 12:24:27 +0200

    additions

--- src/_init.js
+++ src/_init.js
@@ -84,6 +84,29 @@ Creep.prototype.moveToEx = function(target) {
   }
 };
 
+Creep.prototype.getActiveBodyparts = function (type) {
+    var count = 0;
+    for (var i = this.body.length; i-- > 0;) {
+        if (this.body[i].hits > 0) {
+            if (this.body[i].type === type) {
+                count++;
+            }
+        } else break;
+    }
+    return count;
+};
+
+Creep.prototype.hasActiveBodyparts = function (type) {
+    for (var i = this.body.length; i-- > 0;) {
+        if (this.body[i].hits > 0) {
+            if (this.body[i].type === type) {
+                return true;
+            }
+        } else break;
+    }
+    return false;
+};
+
 /**
  * Extend source
  */
@@ -134,6 +157,32 @@ Structure.prototype.getFreeFields = function() {
   return this.room.memory.maxUpgraders || 0;
 };
 
+/**
+     * Defines a .mineral property for rooms that caches and gives you the mineral object for a room
+	 * Author: Helam
+     */
+    Object.defineProperty(Room.prototype, 'mineral', {
+        get: function() {
+            if (this == Room.prototype || this == undefined)
+                return undefined;
+            if (!this._mineral) {
+                if (this.memory.mineralId === undefined) {
+                    let [mineral] = this.find(FIND_MINERALS);
+                    if (!mineral) {
+                        return this.memory.mineralId = null;
+                    }
+                    this._mineral = mineral;
+                    this.memory.mineralId = mineral.id;
+                } else {
+                    this._mineral = Game.getObjectById(this.memory.mineralId);
+                }
+            }
+            return this._mineral;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    
 // TODO: is this stell needed?
 Spawn.prototype.needsRepair = function() {
   return this.hits < (this.hitsMax - 250);
