6e781a8de84d55074efa94f68b64a1419b8aeb38
utiuti <marcel.uthoff@gmail.com>
2017-06-18 21:05:32 +0200

    activate upgraders

--- src/config.creeps.js
+++ src/config.creeps.js
@@ -73,10 +73,8 @@ module.exports = {
 
     canBuild : function(rc) {
       var controller = rc.getController();
-      // var store = rc.storage.store[RESOURCE_ENERGY] < 20000;
-      // console.log("Updgrade produce: " + store);
-      // return ( controller && controller.my && rc.getCreeps('upgrader').length < 1);
-      return false;
+      return ( controller && controller.my && rc.getCreeps('upgrader').length < 1);
+      //return false;
       // var max = controller.getFreeFields();
       // if ( max > 3 ) {
       //   max = 3;
