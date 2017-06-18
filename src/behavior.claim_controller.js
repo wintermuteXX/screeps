80d0ccc8e17dc4e16d8776a773cb7e85c14ce567
utiuti <marcel.uthoff@gmail.com>
2017-06-18 21:33:21 +0200

    test

--- src/behavior.claim_controller.js
+++ src/behavior.claim_controller.js
@@ -3,6 +3,7 @@ var Behavior = require("_behavior");
 var b = new Behavior("claim_controller");
 
 b.when = function(creep, rc) {
+  console.log("Should I claim controller? " + creep.name);
   return ( creep.room.controller && !creep.room.controller.my);
 };
 
@@ -16,6 +17,8 @@ if (creep.pos.isNearTo(creep.room.controller)) {
             creep.claimController(creep.room.controller);
         }
         else {
+           console.log("I move to controller. Name: " + creep.name);
+ 
             creep.moveToEx(creep.room.controller);
         }
 };
