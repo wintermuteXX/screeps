8f29fece94a2f4cf19fd819098a78dd77d56f92c
Zimt <marcel.uthoff@gmail.com>
2017-06-22 11:49:51 +0200

    withdraw

--- src/behavior.get_energy_spawn.js
+++ src/behavior.get_energy_spawn.js
@@ -39,7 +39,7 @@ b.work = function(creep, rc) {
     if ( !creep.pos.isNearTo(target) ) {
       creep.moveToEx(target);
     } else {
-      target.transferEnergy(creep);
+      creep.withdraw(target, RESOURCE_ENERGY);
     }
   }
 
