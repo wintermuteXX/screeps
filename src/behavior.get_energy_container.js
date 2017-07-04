a034faa71922eb3217ed3b4c062792ae57311491
Zimt <marcel.uthoff@gmail.com>
2017-07-04 16:17:02 +0200

    fix

--- src/behavior.get_energy_container.js
+++ src/behavior.get_energy_container.js
@@ -3,7 +3,14 @@ var Behavior = require("_behavior");
 var b = new Behavior("get_energy_container");
 
 b.when = function (creep, rc) {
- return (creep.energy === 0 && _.filter(rc.find(FIND_STRUCTURES), function (f) { return f.structureType === STRUCTURE_CONTAINER && f.energy > 300; }));
+
+var containersWithEnergy = rc.find(FIND_STRUCTURES, {
+    filter: (i) => i.structureType == STRUCTURE_CONTAINER &&
+                   i.store[RESOURCE_ENERGY] > 300 });
+
+ // return (creep.energy === 0 && _.filter(rc.find(FIND_STRUCTURES), function (f) { return f.structureType === STRUCTURE_CONTAINER && f.energy > 300; }));
+return (creep.energy === 0 && containersWithEnergy);
+
 };
 
 b.completed = function (creep, rc) {
@@ -13,10 +20,12 @@ b.completed = function (creep, rc) {
 
 b.work = function (creep, rc) {
   var target = creep.getTarget();
- console.log("gec: Work target -> " + target);
   if (target === null) {
-    var target = _.filter(rc.find(FIND_STRUCTURES), function (f) { return f.structureType === STRUCTURE_CONTAINER && f.energy > 300; });
-    target = target[0];
+    // var target = _.filter(rc.find(FIND_STRUCTURES), function (f) { return f.structureType === STRUCTURE_CONTAINER && f.energy > 300; });
+    target = rc.find(FIND_STRUCTURES, {
+    filter: (i) => i.structureType == STRUCTURE_CONTAINER &&
+                   i.store[RESOURCE_ENERGY] > 300 });
+    
     if (target) {
     creep.target = target.id;
     }
