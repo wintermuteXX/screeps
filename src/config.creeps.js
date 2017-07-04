c4940c61f3b052b81cc69228849a09d461199778
Zimt <marcel.uthoff@gmail.com>
2017-07-04 15:34:51 +0200

    fix

--- src/config.creeps.js
+++ src/config.creeps.js
@@ -64,7 +64,7 @@ module.exports = {
       [MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, CARRY, CARRY, CARRY, CARRY, CARRY, CARRY]
     ],
 
-    behaviors: ["get_energy_dropped", "get_energy_link", "get_energy_storage", "get_energy_container", "transfer_energy_extensions", "transfer_energy_spawn", "transfer_energy_tower", "transfer_energy_upgrader", "transfer_energy_storage"]
+    behaviors: ["get_energy_dropped", "get_energy_link", "get_energy_container", "get_energy_storage", "transfer_energy_extensions", "transfer_energy_spawn", "transfer_energy_tower", "transfer_energy_upgrader", "transfer_energy_storage"]
   },
 
   "upgrader": {
