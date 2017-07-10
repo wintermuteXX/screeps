2132f60ab39f16c1e00471f23fd8e209e02d59be
Zimt <marcel.uthoff@gmail.com>
2017-07-10 16:23:48 +0200

    energy <> minerals

--- src/behavior.get_minerals.js
+++ src/behavior.get_minerals.js
@@ -4,8 +4,8 @@ var b = new Behavior("get_minerals");
 
 b.when = function (creep, rc) {
   var droppedMinerals = creep.room.find(FIND_MINERALS);
-  // droppedEnergy = _.filter(droppedEnergy, function (f) { return f.amount > 99 && !f.pos.inRangeTo(creep.room.controller.pos, 3); });
-    
+  droppedMinerals = _.filter(droppedMinerals, function (f) { return f.amount > 99 && !f.resourceType == 'RESOURCE_ENERGY' && f.resourceType == 'RESOURCE_POWER' });
+   
   return (_.sum(creep.carry) === 0 && droppedMinerals && droppedMinerals.length);
 };
 
@@ -20,11 +20,8 @@ b.work = function (creep, rc) {
 
   if (target === null) {
     var droppedMinerals = creep.room.find(FIND_MINERALS);
-    //console.log("Dropped Energy: " + droppedEnergy);
-    // droppedEnergy = _.filter(droppedEnergy, function (f) { return f.amount > 99 && !f.pos.inRangeTo(creep.room.controller.pos, 3); });
-    //console.log("Dropped Energy 100: " + droppedEnergy);
-
-
+    droppedMinerals = _.filter(droppedMinerals, function (f) { return f.amount > 99 && !f.resourceType == 'RESOURCE_ENERGY' && f.resourceType == 'RESOURCE_POWER' });
+  
     if (droppedMinerals.length) {
       target = droppedMinerals[Math.floor(Math.random() * droppedMinerals.length)];
       creep.target = target.id;
