825e4ceff6d819aa25805eac5d93f5fbba0872d2
Zimt <marcel.uthoff@gmail.com>
2017-07-19 08:48:47 +0200

    test

--- src/behavior.transfer_energy_storage.js
+++ src/behavior.transfer_energy_storage.js
@@ -7,15 +7,13 @@ b.when = function (creep, rc) {
   var storage = creep.room.storage;
   if (!storage) return false;
   console.log(storage.store[RESOURCE_ENERGY] < global.getInterval('StoreLevel4'));
-  if ( creep.room.controller.level === 4 && storage.store[RESOURCE_ENERGY] < global.getInterval('StoreLevel4')) { 
-      return true;
-  }
+  if ( creep.room.controller.level === 4 && storage.store[RESOURCE_ENERGY] < global.getInterval('StoreLevel4')) return true;
   if ( creep.room.controller.level === 5 && storage.store[RESOURCE_ENERGY] < global.getInterval('StoreLevel5')) return true;
   if ( creep.room.controller.level === 6 && storage.store[RESOURCE_ENERGY] < global.getInterval('StoreLevel6')) return true;
   if ( creep.room.controller.level === 7 && storage.store[RESOURCE_ENERGY] < global.getInterval('StoreLevel7')) return true;
   if ( creep.room.controller.level === 8 && storage.store[RESOURCE_ENERGY] < global.getInterval('StoreLevel8')) return true;
-  
-  return (!!storage);
+  console.log("Jetzt kommt false :-)");
+  return (false);
 };
 
 b.completed = function (creep, rc) {
