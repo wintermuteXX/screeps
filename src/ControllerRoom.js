a237f682d88bd3254e604a27edbb66c85f02e2dd
Zimt <marcel.uthoff@gmail.com>
2017-07-25 13:11:40 +0200

    test

--- src/ControllerRoom.js
+++ src/ControllerRoom.js
@@ -262,6 +262,27 @@ ControllerRoom.prototype._getStructures = function (filter) {
 	return result;
 };
 
+
+const structureTimeToSaveFor = 13;
+	ControllerRoom.prototype.getStructures2 =
+    function (structureType, filter) {
+    if (!global[this.name]) global[this.name] = {};
+        if (!global[this.name].structures || Game.time-global[this.name].structures.lc > structureTimeToSaveFor) {
+            global[this.name].structures = {lc: Game.time};
+
+            var grouped = _.groupBy(this.find(FIND_STRUCTURES), (s) => s.structureType);
+            var mappedGroup = {};
+
+            for (let group in grouped) mappedGroup[group] = _.map(grouped[group], (s) => {return s ? s.id : null});
+
+            global[this.name].structures.structures = mappedGroup;
+        }
+
+        var needed = global[this.name].structures.structures[structureType].map(Game.getObjectById).filter(obj => obj !== null);
+        return filter ? _.filter(needed, filter) : needed;
+	};
+	
+
 ControllerRoom.prototype.analyse = function () {
 	// TODO: Hard coded CPU Limit? No way
 	if (Game.cpuLimit <= 100) return;
