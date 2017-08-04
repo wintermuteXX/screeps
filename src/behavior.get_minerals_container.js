030c52fe86b76a7527fa6932982a73f675d7e7f1
Zimt <marcel.uthoff@gmail.com>
2017-08-04 14:08:31 +0200

    ttest

--- src/behavior.get_minerals_container.js
+++ src/behavior.get_minerals_container.js
@@ -6,17 +6,20 @@ b.when = function (creep, rc) {
   var containers = _.filter(rc.find(FIND_STRUCTURES), function (f) { return f.structureType === STRUCTURE_CONTAINER});
   containers = _.filter(containers, function (f) { return !f.store['energy']; });
   return (creep.energy === 0 && containers);
+}
 
 b.completed = function (creep, rc) {
   var target = Game.getObjectById(creep.target);
-  return (target === null || creep.energy > 0);
+  return (target === null || _.sum(creep.carry) > 0);
 };
 
 b.work = function (creep, rc) {
+  console.log("Getting minerals");
   var target = creep.getTarget();
   if (target === null) {
-    var target = _.filter(rc.find(FIND_STRUCTURES), function (f) { return f.structureType === STRUCTURE_CONTAINER});
-    target = _.filter(containers, function (f) { return !f.store['energy']; });
+    target = _.filter(rc.find(FIND_STRUCTURES), function (f) { return f.structureType === STRUCTURE_CONTAINER});
+    target = _.filter(target, function (f) { return !f.store['energy']; });
+    console.log("Das ist mein Ziel: " + target);
     if (target) {
     creep.target = target.id;
     }
