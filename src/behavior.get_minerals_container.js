ad5fa72bbc27fd9d6b83f229df2e31613184c176
Zimt <marcel.uthoff@gmail.com>
2017-08-08 16:46:37 +0200

    check

--- src/behavior.get_minerals_container.js
+++ src/behavior.get_minerals_container.js
@@ -3,10 +3,8 @@ var Behavior = require("_behavior");
 var b = new Behavior("get_minerals_container");
 
 b.when = function (creep, rc) {
-  // var containers = _.filter(rc.find(FIND_STRUCTURES), function (f) { return f.structureType === STRUCTURE_CONTAINER});
-  // containers = _.filter(containers, function (f) { return !f.store['energy']; });
   var containers = rc.getMineralContainer();
-  return (creep.energy === 0 && containers);
+  return (_.sum(creep.carry) === 0 && containers);
 }
 
 b.completed = function (creep, rc) {
@@ -15,12 +13,9 @@ b.completed = function (creep, rc) {
 };
 
 b.work = function (creep, rc) {
-  console.log("Hole Mineralien " + creep.name);
   var target = creep.getTarget();
   if (target === null) {
     target = rc.getMineralContainer();
-    // target = _.filter(rc.find(FIND_STRUCTURES), function (f) { return f.structureType === STRUCTURE_CONTAINER});
-    // target = _.filter(target, function (f) { return !f.store['energy']; });
     if (target) {
     creep.target = target.id;
     }
