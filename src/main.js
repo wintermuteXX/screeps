650d6dc267fa8f17251938af207dd4c1a93c9372
Zimt <marcel.uthoff@gmail.com>
2017-09-07 13:28:09 +0200

    g

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 // Game.spawns.Eden.createCreep([WORK, CARRY, MOVE, CARRY, MOVE], null, {role: 'builder'});
 // Game.spawns.Eden.createCreep([MOVE, CARRY, MOVE, CARRY, MOVE, CARRY], null, {role: 'transporter'});
-// Game.spawns.Winterfell.createCreep([MOVE, CARRY, WORK, WORK, WORK, WORK], null, {role: 'upgrader'});
+// Game.spawns.Southgate.createCreep([MOVE, CARRY, MOVE, CARRY, WORK, WORK, WORK, WORK, WORK, WORK], null, {role: 'upgrader'});
 
 var profiler = require('screeps-profiler');
 var Traveler = require('Traveler');
