f5ab1aa7a3be874b9cf29814dcf5ec4863ca1301
Zimt <marcel.uthoff@gmail.com>
2017-09-21 08:27:23 +0200

    d

--- src/behavior.get_resources.js
+++ src/behavior.get_resources.js
@@ -19,9 +19,13 @@ b.work = function (creep, rc) {
     var resources = creep.room.memory._droppedResources;
     for (var resource in resources) {
       if (resources[resource].amount > 0) {
-        creep.room.memory._droppedResources[resource].amount = creep.room.memory._droppedResources[resource].amount - _.sum(creep.carry);
+        // console.log(creep.room.memory._droppedResources[resource].amount);
+        // console.log(creep.carryCapacity);
+        creep.room.memory._droppedResources[resource].amount -=  creep.carryCapacity;
+        // console.log(creep.room.memory._droppedResources[resource].amount);
         target = resources[resource].id;
         creep.target = target;
+        target = creep.getTarget();
         console.log("Creep " + creep.name + " has target " + resources[resource].resourceType);
         creep.memory.resourceType = resources[resource].resourceType;
         creep.memory.structure = resources[resource].structure;
@@ -35,18 +39,16 @@ b.work = function (creep, rc) {
       let result = creep.pickup(target, creep.memory.resourceType);
       switch (result) {
         case OK:
-          creep.target = null;
-          creep.memory.resourceType = null;
-          creep.memory.structure = null;
-          break;
-        case ERR_NOT_IN_RANGE:
-          creep.travelTo(target);
-          break;
         case ERR_NOT_ENOUGH_RESOURCES:
           creep.target = null;
           creep.memory.resourceType = null;
           creep.memory.structure = null;
           break;
+        
+          case ERR_NOT_IN_RANGE:
+          creep.travelTo(target);
+          break;
+          
         default:
           console.log(`unknown result from (creep ${creep}).pickup(${target}): ${result}`);
       }
@@ -54,17 +56,13 @@ b.work = function (creep, rc) {
       let result = creep.withdraw(target, creep.memory.resourceType);
       switch (result) {
         case OK:
-          creep.target = null;
+        case ERR_NOT_ENOUGH_RESOURCES:
+        creep.target = null;
           creep.memory.resourceType = null;
           creep.memory.structure = null;
           break;
         case ERR_NOT_IN_RANGE:
           creep.travelTo(target);
-          break;
-        case ERR_NOT_ENOUGH_RESOURCES:
-          creep.target = null;
-          creep.memory.resourceType = null;
-          creep.memory.structure = null;
           break;
         default:
           console.log(`unknown result from (creep ${creep}).withdraw(${target}): ${result}`);
