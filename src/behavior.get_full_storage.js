3a6f13232f220d5da0b530c502cb0b191372d7d7
Zimt <marcel.uthoff@gmail.com>
2017-10-05 09:23:07 +0200

    fix

--- src/behavior.get_full_storage.js
+++ src/behavior.get_full_storage.js
@@ -23,7 +23,7 @@ b.work = function (creep, rc) {
       } else {
         _.each(target.store, function (amount, resourceType) {
               if (amount > 20000) {
-              console.log("In Terminal: " + creep.terminal.store[resourceType]);
+              console.log("In Terminal: " + creep.room.terminal.store[resourceType]);
               // if (amount > 20000 && creep.terminal.store[resourceType] < 100000) {
               
                 // console.log("Get full storage: " + amount, resourceType, target.room.name);
