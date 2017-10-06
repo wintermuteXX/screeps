6b489096fd79efb19d40903dae5f572bd6267419
Zimt <marcel.uthoff@gmail.com>
2017-10-06 15:48:40 +0200

    x

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 // Game.spawns.Eden.createCreep([WORK, CARRY, MOVE, CARRY, MOVE], null, {role: 'builder'});
 // Game.spawns.Eden.createCreep([MOVE, CARRY, MOVE, CARRY, MOVE, CARRY], null, {role: 'transporter'});
 // Game.spawns.Southgate.createCreep([MOVE, CARRY, MOVE, CARRY, WORK, WORK, WORK, WORK, WORK, WORK], null, {role: 'upgrader'});
+// Game.market.createOrder(ORDER_BUY, RESOURCE_ENERGY, 0.01, 30000, "E68S47");
+// Game.market.deal('59d1e4719b0a8a64bbfdd5dc', 30000, "E68S47");
 
 var profiler = require('screeps-profiler');
 var Traveler = require('Traveler');
