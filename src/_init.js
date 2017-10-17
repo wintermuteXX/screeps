1c83d8ba30b12cba066085931e99fce20914e5e0
Zimt <marcel.uthoff@gmail.com>
2017-10-17 11:56:48 +0200

    memory

--- src/_init.js
+++ src/_init.js
@@ -187,23 +187,23 @@ if (Creep && Creep.prototype && !Creep.prototype.behavior) {
   Object.defineProperty(Source.prototype, 'memory', {
     configurable: true,
     get: function() {
-        if(_.isUndefined(Memory.mySourcesMemory)) {
-            Memory.mySourcesMemory = {};
+        if(_.isUndefined(Memory.rooms[this.room.name].sources)) {
+            Memory.rooms[this.room.name].sources = {};
         }
-        if(!_.isObject(Memory.mySourcesMemory)) {
+        if(!_.isObject(Memory.rooms[this.room.name].sources)) {
             return undefined;
         }
-        return Memory.mySourcesMemory[this.id] = 
-                Memory.mySourcesMemory[this.id] || {};
+        return Memory.rooms[this.room.name].sources[this.id] = 
+                Memory.rooms[this.room.name].sources[this.id] || {};
     },
     set: function(value) {
-        if(_.isUndefined(Memory.mySourcesMemory)) {
-            Memory.mySourcesMemory = {};
+        if(_.isUndefined(Memory.rooms[this.room.name].sources)) {
+            Memory.rooms[this.room.name].sources = {};
         }
-        if(!_.isObject(Memory.mySourcesMemory)) {
+        if(!_.isObject(Memory.rooms[this.room.name].sources)) {
             throw new Error('Could not set source memory');
         }
-        Memory.mySourcesMemory[this.id] = value;
+        Memory.rooms[this.room.name].sources[this.id] = value;
     }
 });
 
