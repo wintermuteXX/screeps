4939ebde17db27d126e1f99cb1a0983cc6056e30
Zimt <marcel.uthoff@gmail.com>
2017-11-03 09:37:06 +0100

    fix

--- src/_init.js
+++ src/_init.js
@@ -373,10 +373,10 @@ if (Creep && Creep.prototype && !Creep.prototype.behavior) {
 
   Room.prototype.getResourceAmount = function (res) {
     var amount = 0;
-    if (this.storage.store[res]) {
+    if (this.storage && this.storage.store[res]) {
       amount += this.storage.store[res];
     }
-    if (this.terminal.store[res]) {
+    if (this.terminal && this.terminal.store[res]) {
       amount += this.terminal.store[res];
     }
     return amount;
