a968806ed798e1e964299e7c54c882c6bf19e0e8
Zimt <marcel.uthoff@gmail.com>
2017-11-07 11:34:28 +0100

    test

--- src/behavior.transfer_resources.js
+++ src/behavior.transfer_resources.js
@@ -19,17 +19,13 @@ b.work = function (creep, rc) {
     creep.target = null;
     for (var resource in resources) {
       if (resources[resource].amount > 0 && creep.memory.resourceType == resources[resource].resourceType) {
-        // console.log(creep.room.memory.QueueAvailableResources[resource].amount);
-        // console.log(creep.carryCapacity);
-        //creep.room.memory.QueueAvailableResources[resource].amount -= creep.carryCapacity;
-        // console.log(creep.room.memory.QueueAvailableResources[resource].amount);
         creep.target = resources[resource].id;
         target = creep.getTarget();
         console.log("Creep " + creep.pos + " will deliver " + resources[resource].resourceType);
-        // creep.memory.resourceType = resources[resource].resourceType;
         break;
       }
     }
+    // Backup if no target found -> Terminal
     if(!target && creep.room.terminal) {creep.target = creep.room.terminal.id; target = creep.getTarget();}
   }
 
@@ -39,7 +35,7 @@ b.work = function (creep, rc) {
     switch (result) {
       case OK:
       case ERR_NOT_ENOUGH_RESOURCES:
-      creep.memory.resourceType = null;
+      if (creep.energy === 0) creep.memory.resourceType = null;
       case ERR_FULL:
         creep.target = null;
         creep.memory.structure = null;
