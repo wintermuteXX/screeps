c7ad548891a43fbce40caeb8d81a47f73cc9c417
Zimt <marcel.uthoff@gmail.com>
2017-11-20 08:26:09 +0100

    stats test

--- src/main.js
+++ src/main.js
@@ -9,6 +9,29 @@
 var profiler = require('screeps-profiler');
 var Traveler = require('Traveler');
 
+// test
+Memory.stats['cpu.getUsed'] = Game.cpu.getUsed()
+Memory.stats['cpu.limit'] = Game.cpu.limit
+Memory.stats['cpu.bucket'] = Game.cpu.bucket
+Memory.stats['gcl.progress'] = Game.gcl.progress
+Memory.stats['gcl.progressTotal'] = Game.gcl.progressTotal
+Memory.stats['gcl.level'] = Game.gcl.level
+_.forEach(Object.keys(Game.rooms), function(roomName){
+  let room = Game.rooms[roomName]
+  if(room.controller && room.controller.my){
+  Memory.stats['rooms.' + roomName + '.rcl.level'] = room.controller.level
+  Memory.stats['rooms.' + roomName + '.rcl.progress'] = room.controller.progress
+  Memory.stats['rooms.' + roomName + '.rcl.progressTotal'] = room.controller.progressTotal
+  Memory.stats['rooms.' + roomName + '.spawn.energy'] = room.energyAvailable
+  Memory.stats['rooms.' + roomName + '.spawn.energyTotal'] = room.energyCapacityAvailable
+  if(room.storage){
+  Memory.stats['rooms.' + roomName + '.storage.energy'] = room.storage.store.energy
+  }
+  }
+  })
+// test end
+
+
 // profiler.enable();
 module.exports.loop = function () {
   profiler.wrap(function () {
