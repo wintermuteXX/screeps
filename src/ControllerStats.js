e73f04e1618450f3852cb8b1698847d56c4366de
Zimt <marcel.uthoff@gmail.com>
2017-11-21 10:25:11 +0100

    test

--- src/ControllerStats.js
+++ src/ControllerStats.js
@@ -1,25 +1,27 @@
-function doStats() {
+module.exports = {
 
-  console.log("Doing stats");
-  Memory.stats['cpu.limit'] = Game.cpu.limit
-  Memory.stats['cpu.bucket'] = Game.cpu.bucket
-  Memory.stats['gcl.controllerProgress'] = Game.gcl.progress
-  Memory.stats['gcl.controllerProgressTotal'] = Game.gcl.progressTotal
-  Memory.stats['gcl.level'] = Game.gcl.level
-  _.forEach(Object.keys(Game.rooms), function (roomName) {
-    let room = Game.rooms[roomName]
-    if (room.controller && room.controller.my) {
-      Memory.stats['rooms.' + roomName + '.rcl.level'] = room.controller.level
-      Memory.stats['rooms.' + roomName + '.rcl.progress'] = room.controller.progress
-      Memory.stats['rooms.' + roomName + '.rcl.progressTotal'] = room.controller.progressTotal
-      Memory.stats['rooms.' + roomName + '.spawn.energy'] = room.energyAvailable
-      Memory.stats['rooms.' + roomName + '.spawn.energyTotal'] = room.energyCapacityAvailable
-      if (room.storage) {
-        Memory.stats['rooms.' + roomName + '.storage.energy'] = room.storage.store.energy
+  doStats() {
+
+    console.log("Doing stats");
+    Memory.stats['cpu.limit'] = Game.cpu.limit
+    Memory.stats['cpu.bucket'] = Game.cpu.bucket
+    Memory.stats['gcl.controllerProgress'] = Game.gcl.progress
+    Memory.stats['gcl.controllerProgressTotal'] = Game.gcl.progressTotal
+    Memory.stats['gcl.level'] = Game.gcl.level
+    _.forEach(Object.keys(Game.rooms), function (roomName) {
+      let room = Game.rooms[roomName]
+      if (room.controller && room.controller.my) {
+        Memory.stats['rooms.' + roomName + '.rcl.level'] = room.controller.level
+        Memory.stats['rooms.' + roomName + '.rcl.progress'] = room.controller.progress
+        Memory.stats['rooms.' + roomName + '.rcl.progressTotal'] = room.controller.progressTotal
+        Memory.stats['rooms.' + roomName + '.spawn.energy'] = room.energyAvailable
+        Memory.stats['rooms.' + roomName + '.spawn.energyTotal'] = room.energyCapacityAvailable
+        if (room.storage) {
+          Memory.stats['rooms.' + roomName + '.storage.energy'] = room.storage.store.energy
+        }
       }
-    }
-  })
+    })
 
-  Memory.stats['cpu.getUsed'] = Game.cpu.getUsed();
+    Memory.stats['cpu.getUsed'] = Game.cpu.getUsed();
+  }
 }
-module.exports = ControllerStats;
