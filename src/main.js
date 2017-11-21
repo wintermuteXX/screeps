fc01cfbc5ff550535a371f2a0fefec7b054cbf12
Zimt <marcel.uthoff@gmail.com>
2017-11-21 10:13:37 +0100

    stat Test

--- src/main.js
+++ src/main.js
@@ -8,28 +8,7 @@
 
 var profiler = require('screeps-profiler');
 var Traveler = require('Traveler');
-
-// test
-Memory.stats['cpu.limit'] = Game.cpu.limit
-Memory.stats['cpu.bucket'] = Game.cpu.bucket
-Memory.stats['gcl.controllerProgress'] = Game.gcl.progress
-Memory.stats['gcl.controllerProgressTotal'] = Game.gcl.progressTotal
-Memory.stats['gcl.level'] = Game.gcl.level
-_.forEach(Object.keys(Game.rooms), function(roomName){
-  let room = Game.rooms[roomName]
-  if(room.controller && room.controller.my){
-  Memory.stats['rooms.' + roomName + '.rcl.level'] = room.controller.level
-  Memory.stats['rooms.' + roomName + '.rcl.progress'] = room.controller.progress
-  Memory.stats['rooms.' + roomName + '.rcl.progressTotal'] = room.controller.progressTotal
-  Memory.stats['rooms.' + roomName + '.spawn.energy'] = room.energyAvailable
-  Memory.stats['rooms.' + roomName + '.spawn.energyTotal'] = room.energyCapacityAvailable
-  if(room.storage){
-  Memory.stats['rooms.' + roomName + '.storage.energy'] = room.storage.store.energy
-  }
-  }
-  })
-// test end
-
+var stats    = require('ControllerStats');
 
 // profiler.enable();
 module.exports.loop = function () {
@@ -41,5 +20,5 @@ module.exports.loop = function () {
     gc.garbageCollection();
     gc.processRooms();
   })
-  Memory.stats['cpu.getUsed'] = Game.cpu.getUsed();
+  stats.doStats();
 }
