b2c4891ba93c23cd1bf7b6099743f8519b9998a2
Zimt <marcel.uthoff@gmail.com>
2017-11-24 15:06:22 +0100

    püsh

--- src/behavior.transfer_resources.js
+++ src/behavior.transfer_resources.js
@@ -3,8 +3,15 @@ var b = new Behavior("transfer_resources");
 
 b.when = function (creep, rc) {
   if (!creep.room.memory.QueueNeededResources) return false;
-  if (creep.energy ==  0) return false;
-  return true;
+  if (creep.energy == 0) return false;
+
+  var resources = creep.room.memory.QueueNeededResources;
+  for (var resource in resources) {
+    if (resources[resource].amount > 0 && creep.memory.resourceType == resources[resource].resourceType) {
+      return true;
+    }
+  }
+  return false;
 };
 
 b.completed = function (creep, rc) {
@@ -27,15 +34,15 @@ b.work = function (creep, rc) {
     }
     // Backup if no target found -> Terminal
     //if(!target && creep.room.terminal) {creep.target = creep.room.terminal.id; target = creep.getTarget();}
-    }
+  }
 
   if (target) {
-      let result = creep.transfer(target, creep.memory.resourceType);
-  
+    let result = creep.transfer(target, creep.memory.resourceType);
+
     switch (result) {
       case OK:
       case ERR_NOT_ENOUGH_RESOURCES:
-      if (creep.energy === 0) creep.memory.resourceType = null;
+        if (creep.energy === 0) creep.memory.resourceType = null;
       case ERR_FULL:
         creep.target = null;
         creep.memory.structure = null;
@@ -43,7 +50,7 @@ b.work = function (creep, rc) {
       case ERR_NOT_IN_RANGE:
         creep.travelTo(target);
         break;
-      
+
       default:
         console.log(`unknown result from (creep ${creep}).transfer(${target}): ${result}`);
     }
