5e2f6240537023a5a28b4047a99e03e8c4c92108
Marcel Uthoff <marcel.uthoff@starke.de>
2018-10-12 18:06:52 +0200

    mini fix

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
-// Game.spawns.Eden.createCreep([WORK, CARRY, MOVE, CARRY, MOVE], null, {role: 'builder'});
-// Game.spawns.Eden.createCreep([MOVE, CARRY, MOVE, CARRY, MOVE, CARRY], null, {role: 'transporter'});
-// Game.spawns.Southgate.createCreep([MOVE, CARRY, MOVE, CARRY, WORK, WORK, WORK, WORK, WORK, WORK], null, {role: 'upgrader'});
+// Game.spawns.Kassel.spawnCreep([WORK, CARRY, MOVE, CARRY, MOVE], 'Builder_666', {memory: {role: 'builder'}});
+// Game.spawns.Kassel.spawnCreep([MOVE, CARRY, MOVE, CARRY, MOVE, CARRY], 'Transporter_666', {memory: {role: 'transporter'}});
+// Game.spawns.Kassel.spawnCreep([MOVE, CARRY, MOVE, CARRY, WORK, WORK, WORK, WORK, WORK, WORK], 'Upgrader_666', {memory: {role: 'upgrader'}});
 // Game.market.createOrder(ORDER_BUY, RESOURCE_ENERGY, 0.01, 30000, "E68S47");
 // Game.market.deal('59d1e4719b0a8a64bbfdd5dc', 30000, "E68S47");
 // Game.market.changeOrderPrice('59d232ed78c2755738b3105e', 0.14);
