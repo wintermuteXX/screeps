927d78291e7a5adde04383ed275a3aa322d248f4
Marcel Uthoff <marcel.uthoff@starke.de>
2018-10-16 12:27:38 +0200

    spawn update

--- src/ControllerSpawn.js
+++ src/ControllerSpawn.js
@@ -14,6 +14,8 @@ ControllerSpawn.prototype.createCreep = function (role, creepConfig, memory) {
   if (bodyConfig !== null && bodyConfig.length) {
     memory = memory || {};
     memory.role = role;
+    memory.born = Game.time;
+    memory.bornEnergyLevel = this.spawn.room.energyCapacityAvailable;
     result = this.spawn.spawnCreep(bodyConfig, theName, {
       memory: memory
     });
