588c7839c25186f497a21f43a867821f7f728c12
utiuti <marcel.uthoff@gmail.com>
2018-10-22 21:36:00 +0200

    fix

--- src/behavior.miner_harvest.js
+++ src/behavior.miner_harvest.js
@@ -11,38 +11,38 @@ b.completed = function () {
 };
 
 b.work = function (creep, rc) {
-    var source = creep.getTarget();
+  var source = creep.getTarget();
 
-    if (!creep.target) {
-      source = _.find(rc.getSources(), function (s) {
-        return (rc.getCreeps("miner", s.id).length === 0);
-      });
+  if (!creep.target) {
+    source = _.find(rc.getSources(), function (s) {
+      return (rc.getCreeps("miner", s.id).length === 0);
+    });
+  }
+
+  if (source === null) {
+    source = Game.getObjectById(creep.target);
+  }
+
+  if (source !== null) {
+    creep.target = source.id;
+
+    if (creep.container && !(creep.pos.isEqualTo(source.container.pos))) {
+      creep.moveTo(source.container);
+    } else if (!creep.pos.isNearTo(source)) {
+      creep.moveTo(source);
     }
 
-    if (source === null) {
-      source = Game.getObjectById(creep.target);
+    creep.harvest(source);
+    // TODO: Storage + Link in creep memory speichern und benutzen.
+    var link = rc.findNearLink(creep);
+    // TODO: transfer only when full
+    if (link.length) {
+      creep.memory.link = link[0].id;
+      creep.transfer(link[0], RESOURCE_ENERGY);
+    } else {
+      creep.drop(RESOURCE_ENERGY);
     }
+  }
+}
 
-    if (source !== null) {
-      creep.target = source.id;
-
-      if (creep.container && !(creep.pos.isEqualTo(source.container.pos))) {
-        creep.moveTo(source.container);
-      } else if (!creep.pos.isNearTo(source)) {
-        creep.moveTo(source);
-        //}
-
-        creep.harvest(source);
-        // TODO: Storage + Link in creep memory speichern und benutzen.
-        var link = rc.findNearLink(creep);
-        // TODO: transfer only when full
-        if (link.length) {
-          creep.memory.link = link[0].id;
-          creep.transfer(link[0], RESOURCE_ENERGY);
-        } else {
-          creep.drop(RESOURCE_ENERGY);
-        }
-      }
-    };
-
-    module.exports = b;
+module.exports = b;
--- src/config.creeps.js
+++ src/config.creeps.js
@@ -47,21 +47,12 @@ module.exports = {
     },
 
     "transporter": {
-      priority: 1,
+      priority: 3,
       levelMin: 2,
       minParts: 6,
       renew: true,
       body2: [MOVE, CARRY, MOVE, CARRY, MOVE, CARRY, MOVE, CARRY, MOVE, CARRY, MOVE, CARRY, MOVE, CARRY, MOVE, CARRY, MOVE, CARRY, MOVE, CARRY, MOVE, CARRY, MOVE, CARRY, MOVE, CARRY, MOVE, CARRY, MOVE, CARRY, MOVE, CARRY],
-      behaviors: ["get_energy_link ", "
-        get_energy_dropped ", "
-        get_energy_container ", "
-        get_energy_storage ", "
-        get_energy_terminal ", "
-        transfer_energy_extensions ", "
-        transfer_energy_spawn ", "
-        transfer_energy_tower ", "
-        transfer_energy_upgrader ", "
-        transfer_energy_storage "],
+      behaviors: ["get_energy_link", "get_energy_dropped", "get_energy_container", "get_energy_storage", "get_energy_terminal", "transfer_energy_extensions", "transfer_energy_spawn", "transfer_energy_tower", "transfer_energy_upgrader", "transfer_energy_storage"],
 
         canBuild: function (rc) {
           // var miners = rc.getCreeps('miner');
@@ -74,7 +65,7 @@ module.exports = {
       },
 
       "transporter2": {
-        priority: 1,
+        priority: 3,
         levelMin: 2,
         minParts: 6,
         body2: [MOVE, CARRY, MOVE, CARRY, MOVE, CARRY, MOVE, CARRY, MOVE, CARRY, MOVE, CARRY, MOVE, CARRY, MOVE, CARRY, MOVE, CARRY, MOVE, CARRY, MOVE, CARRY, MOVE, CARRY, MOVE, CARRY, MOVE, CARRY, MOVE, CARRY, MOVE, CARRY],
