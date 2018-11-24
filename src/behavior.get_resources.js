75d447f1704cc41cb58b0a047a3c1f71d4d7efbf
utiuti <marcel.uthoff@gmail.com>
2018-11-24 21:16:55 +0100

    test

--- src/behavior.get_resources.js
+++ src/behavior.get_resources.js
@@ -12,6 +12,7 @@ b.completed = function (creep, rc) {
 };
 
 b.work = function (creep, rc) {
+  Log.warn(`I am running GET RESOURCES in Tick ${Game.time}`, "Creep");
   var target = creep.getTarget();
 
   if (!target) {
