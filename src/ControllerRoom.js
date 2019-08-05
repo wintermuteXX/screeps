041618e01b9f94a6a5a66768cd20f60eba4f8867
Zimt <utiuti@users.noreply.github.com>
2019-08-05 14:29:05 +0200

    remove flags check from center point

--- src/ControllerRoom.js
+++ src/ControllerRoom.js
@@ -835,9 +835,6 @@ ControllerRoom.prototype.centerPoint = function () {
 			});
 			if (structs.length > 0) continue;
 
-			let flags = pos.findInRange(FIND_FLAGS, 4);
-			if (flags.length > 0) continue;
-
 			let terrain = _.filter(this.room.lookForAtArea(LOOK_TERRAIN, y - freeRange, x - freeRange, y + freeRange, x + freeRange, true), (p) => p.type == 'terrain' && p.terrain == 'wall');
 			if (terrain.length > 0) continue;
 
