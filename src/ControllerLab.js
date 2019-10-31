6374fafd1a93a97cc8d5fd2cc2356486d5a2b0ce
Zimt <utiuti@users.noreply.github.com>
2019-10-31 15:02:00 +0100

    test

--- src/ControllerLab.js
+++ src/ControllerLab.js
@@ -17,8 +17,8 @@ ControllerLab.prototype.findLabPartner = function () {
     // You need at least 3 labs for a reaction
     if (noStatusLabs.length >= 3) {
         _.shuffle(noStatusLabs)
-        let test = noStatusLabs.length % 3
-        Log.debug(`calculate status for ${noStatusLabs.length} labs which is modulo ${test}`, "ControllerLab");
+        noStatusLabs.length = noStatusLabs.length - (noStatusLabs.length % 3)
+        Log.debug(`calculate status for ${noStatusLabs.length} labs`, "ControllerLab");
         for (let i = 0; i < noStatusLabs.length; i++) {
 
             if (noStatusLabs[i].pos.inRangeTo(noStatusLabs[i + 1].pos, 2) && noStatusLabs[i].pos.inRangeTo(noStatusLabs[i + 2].pos, 2)) {
