f5bb8f626f015e7f7b69d8314c5136b0abc6e2a7
Zimt <utiuti@users.noreply.github.com>
2019-10-31 14:57:28 +0100

    smol test

--- src/ControllerLab.js
+++ src/ControllerLab.js
@@ -10,18 +10,19 @@ ControllerLab.prototype.findLabPartner = function () {
     for (let i in this.labs) {
         let theLab = this.labs[i];
         if (theLab.memory.status == undefined || theLab.memory.status == null) {
-            // Log.debug(`has a lab ${theLab} with no status`, "ControllerLab");
             noStatusLabs.push(theLab);
         }
     }
 
+    // You need at least 3 labs for a reaction
     if (noStatusLabs.length >= 3) {
-        Log.debug(`calculate status for ${noStatusLabs.length} labs`, "ControllerLab");
         _.shuffle(noStatusLabs)
+        let test = noStatusLabs.length %= 3
+        Log.debug(`calculate status for ${noStatusLabs.length} labs which is modulo ${test}`, "ControllerLab");
         for (let i = 0; i < noStatusLabs.length; i++) {
 
             if (noStatusLabs[i].pos.inRangeTo(noStatusLabs[i + 1].pos, 2) && noStatusLabs[i].pos.inRangeTo(noStatusLabs[i + 2].pos, 2)) {
-                Log.debug(`${noStatusLabs[i]} is in Range to ${noStatusLabs[i+2]} and ${noStatusLabs[i+2]}`, "ControllerLab");
+                Log.debug(`${noStatusLabs[i]} is in Range to ${noStatusLabs[i+1]} and ${noStatusLabs[i+2]}`, "ControllerLab");
 
             }
         }
