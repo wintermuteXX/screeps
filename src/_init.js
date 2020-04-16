036e144ab9af00f41b1b1756d05acdd5b4792d2e
Zimt <utiuti@users.noreply.github.com>
2020-04-16 21:40:04 +0200

    fix room.name

--- src/_init.js
+++ src/_init.js
@@ -362,7 +362,7 @@ Creep.prototype.toString = function (htmlLink = true) {
     var onClick = '';
     if (this.id) onClick += `angular.element('body').injector().get('RoomViewPendingSelector').set('${this.id}');` +
       `angular.element($('body')).scope().$broadcast('roomObjectSelected', _.filter(angular.element(document.getElementsByClassName('room ng-scope')).scope().Room.objects, (o)=>o._id==='${this.id}')[0]);`
-    return `<a href="#!/room/${Game.shard.name}/${this.room}" onClick="${onClick}">[${(this.name ? this.name : this.id)}]</a>`;
+    return `<a href="#!/room/${Game.shard.name}/${this.room.name}" onClick="${onClick}">[${(this.name ? this.name : this.id)}]</a>`;
   }
   return `[${(this.name ? this.name : this.id)}]`;
 };
