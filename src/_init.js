6fa0b4672cf35273ee42224707242bdad80a1586
utiuti <marcel.uthoff@gmail.com>
2020-04-22 22:16:29 +0200

    toString spawn

--- src/_init.js
+++ src/_init.js
@@ -373,7 +373,13 @@ Structure.prototype.toString = function (htmlLink = true) {
 };
 
 StructureSpawn.prototype.toString = function (htmlLink = true) {
-  return `[structure (${this.structureType}) #${this.id} ${this.pos.toString(htmlLink, this.id)}]`;
+  if (htmlLink) {
+    var onClick = '';
+    if (this.id) onClick += `angular.element('body').injector().get('RoomViewPendingSelector').set('${this.id}');` +
+      `angular.element($('body')).scope().$broadcast('roomObjectSelected', _.filter(angular.element(document.getElementsByClassName('room ng-scope')).scope().Room.objects, (o)=>o._id==='${this.id}')[0]);`
+    return `<a href="#!/room/${Game.shard.name}/${this.room.name}" onClick="${onClick}">[${(this.name ? this.name : this.id)}]</a>`;
+  }
+  return `[(${this.structureType}) #${this.id}]`;
 };
 
 // REMOVE
