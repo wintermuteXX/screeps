502610314151515f492b6decfb3ad5cef4d6ead8
wintermuteXX <marcel.uthoff.@gmail.com>
2022-04-11 09:08:36 +0200

    reorder stuff

--- src/main.js
+++ src/main.js
@@ -1,37 +1,11 @@
-/* 
-
-Game.spawns.Southtown.spawnCreep([WORK, CARRY, MOVE, CARRY, MOVE], 'Builder_666', {memory: {role: 'builder'}});
-Game.market.createOrder(ORDER_BUY, RESOURCE_ENERGY, 0.01, 30000, "E68S47");
-Game.market.deal('59d1e4719b0a8a64bbfdd5dc', 30000, "E68S47");
-
-grunt screeps --server=season
-
-Log.warn(`The creep spawns?: ${creep.room.spawns[0].spawning} `, "Spawning")
-Log.LEVEL_DEBUG = 0; Log.LEVEL_INFO = 1; Log.LEVEL_WARN = 2; Log.LEVEL_ERROR = 3; Log.LEVEL_SUCCESS = 4;
-
-todo-tree.tags
-**  TODO,FIXME,TEST,BUG,REMOVE,LONGTERM
-
-Game.profiler.profile(ticks, [functionFilter]);
-Game.profiler.email(ticks, [functionFilter]);
-*/
-
-// LONGTERM Remote Mining (there was a formula for calculation if RM makes sense)
-// LONGTERM Harvest Power
-// LONGTERM test attack behavior
-// LONGTERM spawn defenders if attacked
-// LONGTERM Boost creeps
-// LONGTERM Allow creeps to transport more then 1 resource 
-// LONGTERM New Task "recycle" for e.g. mineral miner
-
 var profiler = require('screeps-profiler');
 var Traveler = require('Traveler');
 global.Log = require('Log');
 var stats = require('ControllerStats');
 // var helper = require('helperRoomResources')
 // var visual = require('RoomVisual');
-
 // profiler.enable();
+
 module.exports.loop = function () {
   profiler.wrap(function () {
     // Main.js logic should go here.
@@ -48,3 +22,11 @@ module.exports.loop = function () {
   }
   stats.doStats();
 }
+
+// LONGTERM Remote Mining (there was a formula for calculation if RM makes sense)
+// LONGTERM Harvest Power
+// LONGTERM test attack behavior
+// LONGTERM spawn defenders if attacked
+// LONGTERM Boost creeps
+// LONGTERM Allow creeps to transport more then 1 resource 
+// LONGTERM New Task "recycle" for e.g. mineral miner
