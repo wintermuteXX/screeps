98a08e6601916a77840acdfa62be6425e5bb7087
wintermuteXX <marcel.uthoff.@gmail.com>
2022-04-27 16:23:55 +0200

    Updated Stats

--- src/ControllerStats.js
+++ src/ControllerStats.js
@@ -6,6 +6,7 @@ module.exports = {
       Memory.stats = {}
     }
 
+    // Global values
     Memory.stats['cpu.limit'] = Game.cpu.limit
     Memory.stats['cpu.tickLimit'] = Game.cpu.tickLimit
     Memory.stats['cpu.getUsed'] = Game.cpu.getUsed()
@@ -16,6 +17,13 @@ module.exports = {
     Memory.stats['credits'] = Game.market.credits
     Memory.stats['numberOfCreeps'] = Object.keys(Game.creeps).length
 
+    // Average Marekt prices
+    for (var i in RESOURCES_ALL) {
+      let resource = RESOURCES_ALL[i]
+      Memory.stats["avgPrice." + resource] = Game.market.getHistory(resource)[0].avgPrice
+    }
+
+    // Room progress
     _.forEach(Object.keys(Game.rooms), function (roomName) {
       let room = Game.rooms[roomName]
       if (room.controller && room.controller.my) {
@@ -24,8 +32,9 @@ module.exports = {
         Memory.stats['rooms.' + roomName + '.rcl.progressTotal'] = room.controller.progressTotal
         Memory.stats['rooms.' + roomName + '.spawn.energy'] = room.energyAvailable
         Memory.stats['rooms.' + roomName + '.spawn.energyTotal'] = room.energyCapacityAvailable
-        if (room.storage) {
 
+        // Resources in Stores
+        if (room.storage) {
           _.forEach(room.storage.store, (quantity, item) => {
             Memory.stats['rooms.' + roomName + ".storage." + item] = quantity;
           })
@@ -36,6 +45,13 @@ module.exports = {
             Memory.stats['rooms.' + roomName + ".terminal." + item] = quantity;
           })
         }
+
+        if (room.factory) {
+          _.forEach(room.factory.store, (quantity, item) => {
+            Memory.stats['rooms.' + roomName + ".factory." + item] = quantity;
+          })
+        }
+
       }
     })
   }
